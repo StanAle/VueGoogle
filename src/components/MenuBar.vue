ab93948c186f99cc7726d4869456a9ebc7851dc8
AzureAD\Alexandra-GeorgianaS <alexandra.stan@principal33.com>
2021-05-05 10:42:30 +0300

    Added the vue project and the MenuBar component

---
+++ src/components/MenuBar.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="menu_bar">
+    <div class="menu_items">
+      <div class="menu_item">
+        <a
+          class="gb_g"
+          data-pid="23"
+          href="https://mail.google.com/mail/&amp;ogbl"
+          target="_top"
+          >Gmail</a
+        >
+      </div>
+      <div class="menu_item">
+        <a
+          class="gb_g"
+          data-pid="2"
+          href="https://www.google.ro/imghp?hl=ro&amp;ogbl"
+          target="_top"
+          >Imagini</a
+        >
+      </div>
+      <div class="menu_item">
+        <a
+          class="menu_item all_apps_icon"
+          aria-label="Aplicații Google"
+          href="https://www.google.ro/intl/ro/about/products"
+          aria-expanded="false"
+          role="button"
+          tabindex="0"
+        >
+          <svg class="all_apps_icon" focusable="false" viewbox="0 0 24 24">
+            <path
+              d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"
+            ></path></svg
+        ></a>
+      </div>
+      <div class="menu_item">
+        <img src="images/profile-user.png" class="profile_user_img" />
+      </div>
+    </div></div
+></template>
+
+<script>
+export default {
+  name: "HelloWorld",
+};
+</script>
